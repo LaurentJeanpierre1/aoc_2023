e715492f21e55e7ac7aa54a8903820948aec9161
Jeanpierre <laurent.jeanpierre@unicaen.fr>
2023-12-03 06:40:18 +0100

    day3

--- src/main/kotlin/day03/Day03.kt
+++ src/main/kotlin/day03/Day03.kt
@@ -6,20 +6,125 @@ class Day03(fileName: String, isTest: Boolean): Day(fileName, isTest) {
     constructor(day: Int, isTest: Boolean) : this (makeFileName(day, isTest), isTest)
     
     override fun part1(data: Sequence<String>): Long {
-        TODO("Not yet implemented")
+        val lines = data.toList()
+        var sum = 0L
+        for ((idx,line) in lines.withIndex()) {
+            val matches = Regex("\\d+").findAll(line)
+            for (match in matches) {
+                val range = match.range
+                var added = false
+                if (range.first != 0 && line[range.first-1] != '.') {
+                    sum += match.value.toLong()
+                    if (isTest) println("${match.value} added because ${line[range.first-1]} < ")
+                    continue
+                }
+                if (range.endInclusive != line.lastIndex && line[range.endInclusive+1] != '.') {
+                    sum += match.value.toLong()
+                    if (isTest) println("${match.value} added because ${line[range.endInclusive+1]} > ")
+                    continue
+                }
+                if (idx != 0) {
+                    val prev = lines[idx - 1]
+                    for (i in (range.first-1..range.endInclusive+1).intersect(prev.indices)) {
+                        if (prev[i] != '.' && !prev[i].isDigit()) {
+                            added = true
+                            if (isTest) println("${match.value} added because ${prev[i]} ^[$i] ")
+                            sum += match.value.toLong()
+                            break
+                        }
+                    }
+                }
+                if (! added && idx != lines.lastIndex) {
+                    val next = lines[idx + 1]
+                    for (i in (range.first-1..range.endInclusive+1).intersect(next.indices)) {
+                        if (next[i] != '.' && !next[i].isDigit()) {
+                            if (isTest) println("${match.value} added because ${next[i]} v[$i] ")
+                            sum += match.value.toLong()
+                            break
+                        }
+                    }
+                }
+            }
+        }
+        return sum
     }
+    data class Point(val x: Int, val y:Int){}
 
     override fun part2(data: Sequence<String>): Long {
-        TODO("Not yet implemented")
+        val gears = mutableMapOf<Point, List<Int>>()
+        val lines = data.toList()
+        var sum = 0L
+        for ((idx,line) in lines.withIndex()) {
+            val matches = Regex("\\d+").findAll(line)
+            for (match in matches) {
+                val range = match.range
+                var added = false
+                if (range.first != 0 && line[range.first-1] == '*') {
+                    addGear(idx, gears, range.first - 1, match.value.toInt())
+                    if (isTest) println("${match.value} added because ${line[range.first-1]} < ")
+                    continue
+                }
+                if (range.endInclusive != line.lastIndex && line[range.endInclusive+1] == '*') {
+                    addGear(idx, gears, range.endInclusive + 1, match.value.toInt())
+                    if (isTest) println("${match.value} added because ${line[range.endInclusive+1]} > ")
+                    continue
+                }
+                if (idx != 0) {
+                    val prev = lines[idx - 1]
+                    for (i in (range.first-1..range.endInclusive+1).intersect(prev.indices)) {
+                        if (prev[i] == '*') {
+                            added = true
+                            if (isTest) println("${match.value} added because ${prev[i]} ^[$i] ")
+                            addGear(idx-1, gears, i, match.value.toInt())
+                            break
+                        }
+                    }
+                }
+                if (! added && idx != lines.lastIndex) {
+                    val next = lines[idx + 1]
+                    for (i in (range.first-1..range.endInclusive+1).intersect(next.indices)) {
+                        if (next[i] != '.' && !next[i].isDigit()) {
+                            if (isTest) println("${match.value} added because ${next[i]} v[$i] ")
+                            addGear(idx+1, gears, i, match.value.toInt())
+                            break
+                        }
+                    }
+                }
+            }
+        }
+        for (entry in gears) {
+            if (entry.value.size == 2) {
+                if(isTest) println("Found gear @${entry.key} with ${entry.value}")
+                sum += entry.value[0] * entry.value[1]
+            }
+        }
+        return sum
+    }
+
+    private fun addGear(
+        idx: Int,
+        gears: MutableMap<Point, List<Int>>,
+        x: Int,
+        value: Int
+    ) {
+        val p = Point(x, idx)
+        gears.compute(p) { _, l ->
+            if (l != null) {
+                l.addLast(value)
+                l
+            } else {
+                mutableListOf(value)
+            }
+        }
     }
 }
 
 fun main() {
     val dayTest = Day03(3, isTest=true)
     println("Test part1")
-    check(dayTest.runPart1().also { println("-> $it") } == 8L)
-    //println("Test part2")
-    //check(dayTest.runPart2().also { println("-> $it") } == 2286L)
+    check(dayTest.runPart1().also { println("-> $it") } == 4361L)
+    println("Test part2")
+    check(dayTest.runPart2().also { println("-> $it") } == 467835L)
 
     val day = Day03(3, isTest=false)
     println("Run part1")
