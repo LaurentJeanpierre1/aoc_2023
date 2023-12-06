c5f9b484dfb7852257dba36aa25296758eb87b72
Jeanpierre <laurent.jeanpierre@unicaen.fr>
2023-12-06 06:16:54 +0100

    day6

--- src/main/kotlin/day06/Day06.kt
+++ src/main/kotlin/day06/Day06.kt
@@ -6,18 +6,48 @@ class Day06(fileName: String, isTest: Boolean): Day(fileName, isTest) {
     constructor(day: Int, isTest: Boolean) : this (makeFileName(day, isTest), isTest)
     
     override fun part1(data: Sequence<String>): Long {
-        TODO("Not yet implemented")
+        val lines = data.toList()
+        val times = lines[0].drop(10)
+            .split(' ').filter { it.isNotBlank() }.map { it.toInt() }
+        val distances = lines[1].drop(10)
+            .split(' ').filter { it.isNotBlank() }.map { it.toInt() }
+        var product = 1L
+        for (race in times.indices) {
+            var max = 0
+            for (press in 1..<times[race]) {
+                val dist = press * (times[race]-press)
+                if (isTest) println("Press $press -> dist $dist")
+                if (dist>distances[race]) max++
+            }
+            product *= max
+        }
+        return product
     }
 
     override fun part2(data: Sequence<String>): Long {
-        TODO("Not yet implemented")
+        val lines = data.toList()
+        val times = lines[0].drop(10).replace(" ","")
+            .split(' ').filter { it.isNotBlank() }.map { it.toLong() }
+        val distances = lines[1].drop(10).replace(" ","")
+            .split(' ').filter { it.isNotBlank() }.map { it.toLong() }
+        var product = 1L
+        for (race in times.indices) {
+            var max = 0
+            for (press in 1..<times[race]) {
+                val dist = press * (times[race]-press)
+                if (isTest) println("Press $press -> dist $dist")
+                if (dist>distances[race]) max++
+            }
+            product *= max
+        }
+        return product
     }
 }
 
 fun main() {
     val dayTest = Day06(6, isTest=true)
     println("Test part1")
-    check(dayTest.runPart1().also { println("-> $it") } == 8L)
+    check(dayTest.runPart1().also { println("-> $it") } == 288L)
     //println("Test part2")
     //check(dayTest.runPart2().also { println("-> $it") } == 2286L)
 
