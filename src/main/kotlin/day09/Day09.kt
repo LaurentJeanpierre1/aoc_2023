e8007aa81694f9b8daa9269910c8f23631e75dd5
Jeanpierre <laurent.jeanpierre@unicaen.fr>
2023-12-09 06:33:14 +0100

    day9

--- src/main/kotlin/day09/Day09.kt
+++ src/main/kotlin/day09/Day09.kt
@@ -4,22 +4,49 @@ import Day
 
 class Day09(fileName: String, isTest: Boolean): Day(fileName, isTest) {
     constructor(day: Int, isTest: Boolean) : this (makeFileName(day, isTest), isTest)
-    
+
+    private fun predictNext(values : List<Long>) : Long {
+        val differences = values.windowed(2).map{(v1, v2)-> v2-v1}
+        if (differences.all { it == 0L })
+            return values.last()
+        else {
+            val p = predictNext(differences)
+            return values.last() + p
+        }
+    }
+    private fun predictPrevious(values : List<Long>) : Long {
+        val differences = values.windowed(2).map{(v1, v2)-> v2-v1}
+        if (differences.all { it == 0L })
+            return values.first()
+        else {
+            val p = predictPrevious(differences)
+            return values.first() - p
+        }
+    }
+
     override fun part1(data: Sequence<String>): Long {
-        TODO("Not yet implemented")
+        return data.map { line->
+            predictNext(line.split(' ')
+                .filter { it.isNotBlank() }
+                .map { it.toLong() }).also { if (isTest) println(it) }
+        }.sum()
     }
 
     override fun part2(data: Sequence<String>): Long {
-        TODO("Not yet implemented")
+        return data.map { line->
+            predictPrevious(line.split(' ')
+                .filter { it.isNotBlank() }
+                .map { it.toLong() }).also { if (isTest) println(it) }
+        }.sum()
     }
 }
 
 fun main() {
     val dayTest = Day09(9, isTest=true)
     println("Test part1")
-    check(dayTest.runPart1().also { println("-> $it") } == 8L)
-    //println("Test part2")
-    //check(dayTest.runPart2().also { println("-> $it") } == 2286L)
+    check(dayTest.runPart1().also { println("-> $it") } == 114L)
+    println("Test part2")
+    check(dayTest.runPart2().also { println("-> $it") } == 2L)
 
     val day = Day09(9, isTest=false)
     println("Run part1")
