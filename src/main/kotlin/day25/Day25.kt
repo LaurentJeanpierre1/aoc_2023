d5f562ea69ad6e1282dea024fa151a28eaa9855b
Laurent Jeanpierre <laurent.jeanpierre@unicaen.fr>
2023-12-25 07:43:12 +0100

    Day 25 - thanks Graphviz and Neato

--- src/main/kotlin/day25/Day25.kt
+++ src/main/kotlin/day25/Day25.kt
@@ -1,12 +1,69 @@
 package day25
 
 import Day
+import java.io.File
 
 class Day25(fileName: String, isTest: Boolean): Day(fileName, isTest) {
     constructor(day: Int, isTest: Boolean) : this (makeFileName(day, isTest), isTest)
-    
+
+    private val connected = mutableMapOf<String, MutableList<String>>()
+    fun add(from: String, to: String) {
+        connected.compute(from){_, list ->
+            if (list == null) mutableListOf(to)
+            else {
+                list.add(to)
+                list
+            }
+        }
+    }
     override fun part1(data: Sequence<String>): Long {
-        TODO("Not yet implemented")
+        val dot = File("$fileName.dot").printWriter()
+        dot.println("strict graph{")
+        data.forEach { line->
+            val parts = line.split(' ', ':').filter { it.isNotBlank() }.toList()
+            val main = parts.first()
+            parts.drop(1).forEach {
+                add(main, it)
+                add(it, main)
+                dot.println("$main -- $it")
+            }
+        }
+        dot.println("}")
+        dot.flush()
+        dot.close()
+        //connected.forEach{println(it)}
+        // Run neato on $filename.dot
+        //zpc-xvp
+        //dhl-vfs
+        //nzn-pbq
+        if (!isTest) {
+            connected["zpc"]!!.remove("xvp")
+            connected["dhl"]!!.remove("vfs")
+            connected["nzn"]!!.remove("pbq")
+            connected["xvp"]!!.remove("zpc")
+            connected["vfs"]!!.remove("dhl")
+            connected["pbq"]!!.remove("nzn")
+        } else {
+            connected["hfx"]!!.remove("pzl")
+            connected["bvb"]!!.remove("cmg")
+            connected["nvd"]!!.remove("jqt")
+            connected["pzl"]!!.remove("hfx")
+            connected["cmg"]!!.remove("bvb")
+            connected["jqt"]!!.remove("nvd")
+        }
+        val firstSet = mutableSetOf<String>()
+        var node = connected.keys.first()
+        val queue = java.util.ArrayDeque<String>()
+        queue.add(node)
+        while (queue.isNotEmpty()) {
+            node = queue.poll()
+            if (node !in firstSet) {
+                firstSet += node
+                queue.addAll(connected[node]!!)
+                connected.remove(node)
+            }
+        }
+        return firstSet.size.toLong().also { print("First set is $it large") } * connected.keys.size.toLong().also { print("Second set is $it large") }
     }
 
     override fun part2(data: Sequence<String>): Long {
@@ -17,7 +74,7 @@ class Day25(fileName: String, isTest: Boolean): Day(fileName, isTest) {
 fun main() {
     val dayTest = Day25(25, isTest=true)
     println("Test part1")
-    check(dayTest.runPart1().also { println("-> $it") } == 8L)
+    check(dayTest.runPart1().also { println("-> $it") } == 54L)
     //println("Test part2")
     //check(dayTest.runPart2().also { println("-> $it") } == 2286L)
 
